5e503f00dec41f14ea1cdb023571fe42dc138744
kerawee <weekera@gmail.com>
2022-06-18 18:38:22 +0600

    dadada

--- task-02/index.js
+++ task-02/index.js
@@ -1,3 +1,18 @@
-function test() {
-    
+function test(num) {
+    let arr = [2, 3, 4, 5];
+    for (let i = 0; i < arr.length; i++){
+        if (num % arr[i] == 0){
+            console.log(arr[i]);
+        }
+    }
 }
+test(55);
+function test(number) {
+    let arr = [2, 3, 4, 5];
+    for (let i = 0; i < arr.length; i++){
+        if (number % arr[i] == 0){
+            console.log(arr[i]);
+        }
+    }
+}
+test(30);
